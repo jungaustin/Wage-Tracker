e60421f986bc7400e3c6ede0f6026473c6adf61c
AusJung <137535372+jungaustin@users.noreply.github.com>
2024-09-06 19:40:25 -0700

    Added hours in correct row (ordered by starttime) rather than appending to end

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 import streamlit as st
 from datetime import datetime
 from job import Job
-from streamlit_gsheets import GSheetsConnection
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
 
-# How to authorize google sheets access
+
+# Authorize Google Sheets access
 creds_dict = st.secrets["gcp_service_account"]
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict)
 client = gspread.authorize(creds)
-
-# Standard Date Format
-date_format = "%m/%d/%Y %H:%M:%S"
 sh = client.open("Wages for Streamlit Project")
+date_format = "%m/%d/%Y %H:%M:%S"
+
+def find_job_by_title(title):
+    return next((job for job in st.session_state.jobs if job.title == title), None)
 
 
-
-if "jobs" not in st.session_state:
-    st.session_state.jobs = []
-
-    # Next part is to get all the data already in the spreadsheet
+@st.cache_data
+def load_jobs():
     worksheets = sh.worksheets()
+    jobs = []
     for worksheet in worksheets:
         data = worksheet.get_all_values()
         if len(data) != 0:
@@ -30,99 +29,71 @@ if "jobs" not in st.session_state:
                 start = datetime.strptime(row[0], date_format)
                 end = datetime.strptime(row[1], date_format)
                 temp.add(start, end)
-            if not any(job.title == temp.title for job in st.session_state.jobs):
-                st.session_state.jobs.append(temp)
+            if not any(job.title == temp.title for job in jobs):
+                jobs.append(temp)
+    return jobs
+
+if "jobs" not in st.session_state:
+    st.session_state.jobs = load_jobs()
+    
+if "clock_in_time" not in st.session_state:
+    st.session_state.clock_in_time = None
 
 st.title("Wage Tracker")
-
-if "add_button" not in st.session_state:
-    st.session_state.add_button = False
 
 action = st.radio("What would you like to do?", ("Create a new job", "Add hours to an existing job", "Remove hours from existing job", "Clock In/Out"))
 
 if action == "Create a new job":
-    job_title_input = st.text_input(
-        "Job Title Input",
-        label_visibility="collapsed",
-        disabled=False,
-        placeholder="New Job Title",
-    )
-
-    if job_title_input:
-        wage_input = st.number_input("Enter Hourly Wage", min_value=0.0, format="%.2f", label_visibility="visible", disabled=False, placeholder="Enter Wage Amount")
-        found = False
-        for job in st.session_state.jobs:
-            if job.title == job_title_input:
-                found = True
-        if not found:
-            st.session_state.jobs.append(Job(job_title_input, 0))
-            sheet = sh.add_worksheet(job_title_input, 0, 0)
-            sheet.update_acell('A1', "Job")
-            sheet.update_acell('B1', job_title_input)
-            sheet.update_acell('A2', "Wage")
-            sheet.update_acell('A3', "Start")
-            sheet.update_acell('B3', "End")
-            sheet.update_acell('C3', "Total")
-        if wage_input:
-            for job in st.session_state.jobs:
-                if job.title == job_title_input:
-                    job.changeWage(wage_input)
-                    sheet = sh.worksheet(job_title_input)
-                    sheet.update_acell('B2', wage_input)
+    with st.form(key='create_job_form'):
+        job_title_input = st.text_input("Job Title Input", placeholder="New Job Title")
+        wage_input = st.number_input("Enter Hourly Wage", min_value=0.0, format="%.2f", placeholder="Enter Wage Amount")
+        submit_button = st.form_submit_button("Create Job")
+        if submit_button and job_title_input:
+            if job_title_input not in [job.title for job in st.session_state.jobs]:
+                st.session_state.jobs.append(Job(job_title_input, wage_input))
+                sheet = sh.add_worksheet(job_title_input, 0, 0)
+                sheet.update_acell('A1', "Job")
+                sheet.update_acell('B1', job_title_input)
+                sheet.update_acell('A2', "Wage")
+                sheet.update_acell('A3', "Start")
+                sheet.update_acell('B3', "End")
+                sheet.update_acell('C3', "Total")
+                sheet.update_acell('B2', wage_input)
 
 elif action == "Add hours to an existing job":
     job_titles = [job.title for job in st.session_state.jobs]
     selected_job_title = st.selectbox("Select Job", job_titles)
-
-    selected_job = next((job for job in st.session_state.jobs if job.title == selected_job_title), None)
-    if selected_job:
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            choose_date = st.date_input("What day did you work?", value=None)
-        with col2:
-            clock_in = st.time_input(f"Enter clock in time for {selected_job.title}", value=None)
-        with col3:
-            clock_out = st.time_input(f"Enter clock out time for {selected_job.title}", value=None)
-        add_hours = st.button("Add")
+    selected_job = find_job_by_title(selected_job_title)
+    
+    with st.form(key='add_hours_form'):
+        choose_date = st.date_input("What day did you work?")
+        clock_in = st.time_input(f"Enter clock in time for {selected_job.title}")
+        clock_out = st.time_input(f"Enter clock out time for {selected_job.title}")
+        add_hours = st.form_submit_button("Add")
         if add_hours:
-            st.session_state.add_button = True
-        if st.session_state.add_button:
             start_time = datetime.combine(choose_date, clock_in)
             end_time = datetime.combine(choose_date, clock_out)
             st.write(selected_job.add(start_time, end_time))
             selected_job.addToSheet(start_time, end_time, sh, date_format)
-            st.session_state.add_button = False
-
-## elif action == "Remove hours from existing job":
-    
 
 elif action == "Clock In/Out":
     job_titles = [job.title for job in st.session_state.jobs]
     selected_job_title = st.selectbox("Select Job", job_titles)
-    selected_job = next((job for job in st.session_state.jobs if job.title == selected_job_title), None)
+    selected_job = find_job_by_title(selected_job_title)
     if selected_job:
         col1, col2 = st.columns(2)
         with col1:
             clock_in = st.button("Clock In")
-            clock_in_time = datetime.now()
+            if clock_in:
+                st.session_state.clock_in_time = datetime.now()
+                st.write("Clock in time: " + st.session_state.clock_in_time.strftime("%m-%d-%Y %H:%M"))
         with col2:
             clock_out = st.button("Clock Out")
-            clock_out_time = datetime.now()
-        if clock_in:
-            curr_time = datetime.now()
-            st.write("Clock in time: " + clock_in_time.strftime("%m-%d-%Y %H:%M"))
-        if clock_out:
-            curr_time = datetime.now()
-            st.write("Clock out time: " + clock_out_time.strftime("%m-%d-%Y %H:%M"))
-            if clock_in_time:
-                selected_job.add(clock_in_time, clock_out_time)
-
-if st.session_state.jobs:
-    st.write("## Job List")
-    columns = st.columns(len(st.session_state.jobs))
-    for i, job in enumerate(st.session_state.jobs):
-        with columns[i]:
-            st.write("#### " + f"**{job.title}**: \n #### **${job.wage:.2f}/hr, Total Hours: {getattr(job, 'hours', 0):.2f}** \n")
-            for hours_worked in job.hours_worked[-6:]:
-                st.write("##### " + "Date: " + hours_worked[0].strftime("%m-%d-%Y"))
-                st.write("##### " + hours_worked[0].strftime("%H:%M") + " - " + hours_worked[1].strftime("%H:%M"))
+            if clock_out:
+                if st.session_state.clock_in_time:
+                    clock_out_time = datetime.now()
+                    st.write("Clock out time: " + clock_out_time.strftime("%m-%d-%Y %H:%M"))
+                    selected_job.add(st.session_state.clock_in_time, clock_out_time)
+                    selected_job.addToSheet(st.session_state.clock_in_time, clock_out_time, sh, date_format)
+                    st.write(True)
+                    st.session_state.clock_in_time = None
--- job.py
+++ job.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+
 class Job:
     def __init__(self, title, wage) -> None:
         self.title = title
@@ -12,13 +13,21 @@ class Job:
             else:
                 return False
     def addToSheet(self, startTime : datetime, endTime : datetime, sh, date_format):
+        startTime = startTime.replace(second=0, microsecond=0)
+        endTime = endTime.replace(second=0, microsecond=0)
         sheet = sh.worksheet(self.title)
+        data = sheet.get_all_values()
+        i = 4
+        for i, row in enumerate(data[3:], start=4):
+            temp = row[0]
+            if datetime.strptime(temp, date_format) > startTime:
+                break
         startTimeString = startTime.strftime(date_format)
         endTimeString = endTime.strftime(date_format)
         duration = (endTime-startTime).total_seconds()
         hours = int(duration // 3600)
         minutes = int((duration % 3600) // 60)
-        sheet.append_row([startTimeString, endTimeString, f"{hours}:{minutes:02d}"])
+        sheet.insert_row([startTimeString, endTimeString, f"{hours}:{minutes:02d}"], index = i)
     def changeWage(self, newWage) -> bool:
         self.wage = newWage
         return True
